e7feaed5eb9235f175006c32d86350cb6ed69f87
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2024-05-10 02:54:39 +0300

    Move "uiState" property to BaseViewModel

--- feature/repo/src/main/java/com/example/awesomearchsample/feature/repo/api/repodetails/RepoDetailsViewModel.kt
+++ feature/repo/src/main/java/com/example/awesomearchsample/feature/repo/api/repodetails/RepoDetailsViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.awesomearchsample.feature.repo.api.repodetails
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.awesomearchsample.core.ui.error.UiErrorHandler
+import com.example.awesomearchsample.core.ui.mvvm.BaseViewModel
 import com.example.awesomearchsample.domain.repo.usecase.GetRepoDetailsUseCase
 import com.example.awesomearchsample.feature.repo.api.navigation.RepoScreens
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,12 +16,9 @@ class RepoDetailsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val getRepoDetailsUseCase: GetRepoDetailsUseCase,
     private val errorHandler: UiErrorHandler
-) : ViewModel() {
+) : BaseViewModel<RepoDetailsUiState>(initialUiState = RepoDetailsUiState()) {
 
     private val screenArg: RepoScreens.RepoDetails = requireNotNull(savedStateHandle[ARG_SCREEN])
-
-    private val _uiState = MutableStateFlow(RepoDetailsUiState())
-    val uiState: StateFlow<RepoDetailsUiState> = _uiState
 
     init {
         loadRepoDetails()
@@ -32,24 +27,24 @@ class RepoDetailsViewModel @Inject constructor(
     private fun loadRepoDetails() {
         viewModelScope.launch {
             try {
-                _uiState.update { it.copy(emptyProgress = true) }
+                mutableUiState.update { it.copy(emptyProgress = true) }
                 val repoDetails = getRepoDetailsUseCase.invoke(repoId = screenArg.repoId)
-                _uiState.update { uiState.value.copy(repoDetails = repoDetails) }
+                mutableUiState.update { uiState.value.copy(repoDetails = repoDetails) }
             } catch (e: Exception) {
                 errorHandler.proceed(
                     error = e,
                     errorListener = { uiError ->
-                        _uiState.update { it.copy(emptyError = uiError) }
+                        mutableUiState.update { it.copy(emptyError = uiError) }
                     }
                 )
             } finally {
-                _uiState.update { it.copy(emptyProgress = false) }
+                mutableUiState.update { it.copy(emptyProgress = false) }
             }
         }
     }
 
     fun onErrorActionClick() {
-        _uiState.value = RepoDetailsUiState()
+        mutableUiState.value = RepoDetailsUiState()
         loadRepoDetails()
     }
 

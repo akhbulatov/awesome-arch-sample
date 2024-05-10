8c85e568243d74d352204cdd241cafa609e72e01
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2024-05-10 18:51:13 +0300

    Migrate from Retrofit to Ktor (network)

--- feature/repo/src/main/java/com/example/awesomearchsample/feature/repo/internal/network/RepoApi.kt
+++ feature/repo/src/main/java/com/example/awesomearchsample/feature/repo/internal/network/RepoApi.kt
@@ -1,14 +1,18 @@
 package com.example.awesomearchsample.feature.repo.internal.network
 
-import retrofit2.http.GET
-import retrofit2.http.Path
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
-internal interface RepoApi {
-    @GET("repositories")
-    suspend fun getRepos(): List<RepoNetModel>
+internal class RepoApi(private val httpClient: HttpClient) {
 
-    @GET("repositories/{id}")
-    suspend fun getRepoDetails(
-        @Path("id") repoId: Long
-    ): RepoDetailsNetModel
+    suspend fun getRepos(): List<RepoNetModel> {
+        return httpClient.get(urlString = "repositories")
+            .body()
+    }
+
+    suspend fun getRepoDetails(repoId: Long): RepoDetailsNetModel {
+        return httpClient.get(urlString = "repositories/$repoId")
+            .body()
+    }
 }

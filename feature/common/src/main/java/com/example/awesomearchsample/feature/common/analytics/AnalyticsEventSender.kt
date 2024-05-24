6d33f5d16e962858ada636bc51737fa4b4505046
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2024-05-24 15:51:20 +0300

    Remove unnecessary SendAnalyticsEventUseCase.kt

--- feature/common/src/main/java/com/example/awesomearchsample/feature/common/analytics/AnalyticsEventSender.kt
+++ feature/common/src/main/java/com/example/awesomearchsample/feature/common/analytics/AnalyticsEventSender.kt
@@ -1,17 +1,17 @@
 package com.example.awesomearchsample.feature.common.analytics
 
 import com.example.awesomearchsample.core.analytics.api.AnalyticsEvent
-import com.example.awesomearchsample.core.analytics.api.SendAnalyticsEventUseCase
+import com.example.awesomearchsample.core.analytics.api.AnalyticsRepository
 import com.example.awesomearchsample.core.common.util.AppLogger
 import javax.inject.Inject
 
 class AnalyticsEventSender @Inject constructor(
-    private val sendAnalyticsEventUseCase: SendAnalyticsEventUseCase,
+    private val analyticsRepository: AnalyticsRepository,
 ) {
 
     fun sendEvent(event: AnalyticsEvent) {
         try {
-            sendAnalyticsEventUseCase.invoke(event)
+            analyticsRepository.sendEvent(event)
         } catch (e: Exception) {
             AppLogger.e("Failed to send event=${event.name}: $e")
         }

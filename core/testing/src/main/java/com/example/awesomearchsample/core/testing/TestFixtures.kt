ba4d2fde791a3ef88873cb2bd9c3526051865cf2
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2026-01-28 00:21:13 +0300

    Add forgotten files from core:testing module

---
+++ core/testing/src/main/java/com/example/awesomearchsample/core/testing/TestFixtures.kt
@@ -0,0 +1,33 @@
+package com.example.awesomearchsample.core.testing
+
+import com.example.awesomearchsample.core.common.error.ErrorEntity
+import com.example.awesomearchsample.core.common.error.ErrorHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class FakeErrorHandler(
+    private val errorEntity: ErrorEntity = ErrorEntity.Message("error")
+) : ErrorHandler {
+    override suspend fun getError(throwable: Throwable): ErrorEntity = errorEntity
+
+    override fun recordError(throwable: Throwable) = Unit
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    val testDispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}

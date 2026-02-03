82d892dfe6d620090fe3ca5f0fdc6df3e94e861c
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2026-02-03 17:45:35 +0300

    Apply Kover to all modules

--- build-logic/src/main/kotlin/AwesomeKotlinJvmConventionsPlugin.kt
+++ build-logic/src/main/kotlin/AwesomeKotlinJvmConventionsPlugin.kt
@@ -2,17 +2,34 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension
 
 @Suppress("unused")
 class AwesomeKotlinJvmConventionsPlugin : Plugin<Project> {
 
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("org.jetbrains.kotlin.jvm")
+        pluginManager.apply("org.jetbrains.kotlinx.kover")
 
         extensions.configure(KotlinJvmProjectExtension::class.java) {
             jvmToolchain(17)
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_17)
+            }
+        }
+
+        extensions.configure(KoverProjectExtension::class.java) {
+            reports {
+                filters {
+                    excludes {
+                        classes(
+                            "*.BuildConfig",
+                            "*.R",
+                            "*.R$*",
+                            "*Manifest*"
+                        )
+                    }
+                }
             }
         }
     }

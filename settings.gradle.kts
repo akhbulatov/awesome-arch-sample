2565979bd562a967ba156a625751969c265c4835
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2026-02-04 18:28:46 +0300

    Add @Suppress annotations in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,9 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()

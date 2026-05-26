4b89c27e03c93f1b71cfe6993a8a0d69d7c9e2d4
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2026-05-26 19:22:23 +0300

    Remove unused withState and updateState from BaseViewModel.kt

--- core/ui/src/main/java/com/example/awesomearchsample/core/ui/mvvm/BaseViewModel.kt
+++ core/ui/src/main/java/com/example/awesomearchsample/core/ui/mvvm/BaseViewModel.kt
@@ -4,9 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<S, E : BaseUiEffect>(initialUiState: S) : ViewModel() {
@@ -20,18 +19,6 @@ abstract class BaseViewModel<S, E : BaseUiEffect>(initialUiState: S) : ViewModel
     protected fun emitEffect(effect: E) {
         viewModelScope.launch {
             mutableUiEffect.send(effect)
-        }
-    }
-
-    protected inline fun <reified SS : S> withState(block: (SS) -> Unit) {
-        val state = uiState.value as? SS ?: return
-        block(state)
-    }
-
-    protected inline fun <reified SS : S> updateState(block: (SS) -> S) {
-        mutableUiState.update { current ->
-            val state = current as? SS ?: return@update current
-            block(state)
         }
     }
 }
